f59740879586661a7ef869b20a3e82cf5ef05c4a
Dmitriy Kanaev <kanaev.dm@mail.ru>
2019-12-11 20:30:50 +0300

    add comparator example with lambda

--- src/main/java/FirstLesson.java
+++ src/main/java/FirstLesson.java
@@ -1,10 +1,10 @@
 interface Executable {
-    int execute(int x);
+    int execute(int x, int y);
 }
 
 class Runner {
     public void run (Executable e) {
-        int a = e.execute(3);
+        int a = e.execute(3, 10);
         System.out.println(a);
     }
 }
@@ -25,27 +25,31 @@ public class FirstLesson {
         });*/
 
         Runner runner = new Runner();
+        int a = 4;
         // 1 способ
         //runner.run(new ExecutableImplementation());
 
         // 2 способ
         runner.run(new Executable() {
             @Override
-            public int execute(int x) {
+            public int execute(int x, int y) {
                 System.out.println("Второй способ");
 
-                return x + 1;
+                return x + y;
             }
         });
 
         // 3 способ
-        runner.run((int x) -> {
+        runner.run((int x, int y) -> {
             System.out.println("Третий способ - лямбда выражение");
-
-            return x + 5;
+            // У лямбда выражения нет своей облости видимости! Если переменная а уже есть, ее нельзя создать
+            //int a = 3;
+            return x + y;
         });
+
         // тоже самое что и код выше. слово return опускается
-        runner.run((x) -> x + 5);
+        final int b = 1; // ДОЛЖНА БЫТЬ final, или значение НЕ должно изменяться до использования лямбда выражения!!!
+        runner.run((x, y) -> x + y + b);
 
     }
 }
